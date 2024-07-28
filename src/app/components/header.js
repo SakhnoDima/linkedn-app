837ca4b9f57ba05a4ad6e6e2a69c1cb7316c450c
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2024-07-28 13:48:28 +0300

    add header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,13 +5,16 @@ import { MdLogin } from "react-icons/md";
 import Link from "next/link";
 import LogOut from "./log-out";
 import Image from "next/image";
+import Svg from "/public/lighthouse.svg";
 
 const Header = () => {
   const { data: session } = useSession();
   return (
-    <header className="px-[80px] py-2 border-b-2">
+    <header className="px-[80px] py-2 border-b-2 flex justify-between ">
       <div>
-        <Image src="/public/lighthouse.svg" width={35} height={35} />
+        <Link href="/">
+          <Image src={Svg} width={45} height={45} />
+        </Link>
       </div>
       <div>
         {!session ? (
