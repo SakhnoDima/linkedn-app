3fab081be1aaff55b6630d10f366678abf817cbe
Ваколюк Євгеній 1АКІТ-22б <evgeniy.vakolyuk@gmail.com>
2024-09-02 20:27:36 +0300

    Added telegram notifications

--- src/app/api/services/up-work-cron.js
+++ src/app/api/services/up-work-cron.js
@@ -2,6 +2,11 @@ import cron from "node-cron";
 import { errorList } from "./errors";
 import axios from "axios";
 import { timeCreator, transformQuery } from "../helpers";
+import User from "@/app/lib/user-model";
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token);
 
 class CronUpWorkClass {
   constructor() {
@@ -93,24 +98,59 @@ class CronUpWorkClass {
         try {
           console.log("START");
 
-          // TODO bot req to lambda!!!
+          let chatId;
+          await User.findOne({ _id: userId }, 'chatId')
+            .then(user => {
+              if (user) {
+                chatId = user.chatId;
+                console.log('Found chatId:', chatId);
+              } else {
+                console.log('User not found');
+              }
+            })
+            .catch(error => {
+              console.error('Error finding user:', error);
+            });
 
           await axios
-            .get("http://localhost:3001/get-info", {
-              params: {
-                userId,
-              },
+            .post("https://5zfmj0shmi.execute-api.eu-north-1.amazonaws.com/default/upwork-get-info", {
+              id: userId,
+              chatId: chatId
             })
             .then((response) => {
-              // TODO обробити також на помилку!!!
-              if (response.data.body.isNewMessage) {
+              const newProposals = response.data.newProposals;
+              const newProposalsLength = newProposals.length;
+              if (newProposalsLength !== 0) {
                 console.log("Send message");
+                let proposalsMessage;
+                if (newProposalsLength === 1) {
+                  proposalsMessage = "<b>You have new proposal! ⭐️</b>\n\n";
+                  newProposals.forEach((proposal) => {
+                    proposalsMessage += `Recived ${proposal.time}: <a href="${proposal.link}">${proposal.title}</a>;\n`;
+                  });
+                } else {
+                  proposalsMessage = "<b>You have new proposals! 🚀</b>\n\n";
+                  newProposals.forEach((proposal, index) => {
+                    proposalsMessage += `${index + 1}. Recived ${proposal.time}: <a href="${proposal.link}">${proposal.title}</a>;\n`;
+                  });
+                }
+
+                bot.sendMessage(chatId, proposalsMessage, { parse_mode: 'HTML' })
+                    .then(response => {
+                      console.log("Message sent successfully:\n", proposalsMessage);
+                    })
+                    .catch(error => {
+                      console.error("Error sending message:", error);
+                    });
               } else {
-                console.log("Don`t have new message");
+                console.log("Don`t have new proposals");
               }
-            });
+            })
+              .catch(error => {
+                console.error("Processing telegram message error:", error);
+              });
         } catch (error) {
-          console.log("Error in init cron", error);
+          console.error("Send telegram message error", error);
         }
       });
       tasks[userId] = task;
