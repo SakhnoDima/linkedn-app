013d7018ccee80e6d7b72eb16e97d59baed08e53
Ваколюк Євгеній 1АКІТ-22б <evgeniy.vakolyuk@gmail.com>
2024-09-05 14:09:34 +0300

    New telegram bot

--- src/app/api/telegram-bot/route.js
+++ src/app/api/telegram-bot/route.js
@@ -1,76 +1,53 @@
 import { NextResponse } from "next/server";
 import User from "@/app/lib/user-model";
-import botInstance from "@/app/lib/telegram-bot-init";
 
-export const GET = async (req, res) => {
-  try {
-    botInstance.on("message", async (msg) => {
-      const chatId = msg.chat.id;
+export const POST = async (req) => {
+    try {
+        const data = await req.json();
 
-      if (msg.text && msg.text.toLowerCase().startsWith("/start")) {
-        await botInstance.sendMessage(
-          chatId,
-          `
-This is a bot that will send you notifications about LeadFlow results
-
-❗️ To initialize your account, send a message in the following format:
-id <your id>`
-        );
-      } else if (msg.text && msg.text.toLowerCase().startsWith("id")) {
-        const userId = msg.text.split(" ")[1];
-        if (userId) {
-          console.log("Received user ID:", userId);
-          try {
-            const user = await User.findOneAndUpdate(
-              { _id: userId },
-              { chatId: chatId },
-              { new: true }
+        if (!data.userId || !data.chatId) {
+            return NextResponse.json(
+                { message: "chatId and userId are required" },
+                { status: 400 }
             );
-
-            if (user) {
-              await botInstance.sendMessage(
-                chatId,
-                `✔️ Your Telegram chat is connected to the system\n\nYour Telegram ID: ${chatId}`
-              );
-            } else {
-              await botInstance.sendMessage(
-                chatId,
-                "❌No user found with this ID"
-              );
-            }
-          } catch (error) {
-            console.error("Error saving chatId to database:", error);
-            await botInstance.sendMessage(
-              chatId,
-              "❌ An error occurred while saving your Telegram ID"
-            );
-          }
-        } else {
-          await botInstance.sendMessage(
-            chatId,
-            "❗️ Please provide your ID in the following format:\n id <your id>"
-          );
         }
-      } else if (msg.text && msg.text.toLowerCase().startsWith("hello")) {
-        await botInstance.sendMessage(
-          chatId,
-          `Hello my friend! 
-           🤓 Im your assistant, and going to inform you about new messages!
-           ❗️ To initialize your account, send a message in the following format: id <your id>`
-        );
-      } else {
-        await botInstance.sendMessage(chatId, "❌ Unknown command");
-      }
-    });
 
-    return NextResponse.json(
-      { message: "Telegram bot initialized successful!" },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: error.message || "Server Error" },
-      { status: 500 }
-    );
-  }
+        console.log('User ID:', data.userId, 'Chat ID:', data.chatId);
+
+        const user = await User.findById(data.userId);
+
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 400 }
+            );
+        }
+
+
+        console.log(user.chatId, data.chatId);
+        // Перевіряємо, чи вже є chatId у цього користувача
+        if (user.chatId === String(data.chatId)) {
+            return NextResponse.json(
+                { message: "Chat ID is already linked to this user" },
+                { status: 200 }
+            );
+        }
+
+        // Якщо chatId ще не прив'язаний, оновлюємо його
+        user.chatId = data.chatId;
+        await user.save();
+
+        console.log("Chat ID saved successfully");
+
+        return NextResponse.json(
+            { message: "Chat ID updated successfully" },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error('Error handling request:', error);
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
 };
